da69416b6aa5d23a94e7d031a7841a93ae429422
aleksabozinovic <aleksabozinovic722@gmail.com>
2023-03-12 00:22:22 +0100

    commit

--- js/index.js
+++ js/index.js
@@ -8,11 +8,5 @@ medLinksA.forEach((el) => {
       el.querySelector("a").classList.remove("active__link--medications");
     });
     e.target.classList.add("active__link--medications");
-    // el.classList.remove("active__link--medications");
-    // console.log(el.closest(a));
-    // e.target.classList.toggle("active__link--medications");
-    // if (e.target.classList.contains("active__link--medications")) {
-    // e.target.classList.remove("active__link--medications");
-    // }
   });
 });
